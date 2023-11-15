7f23baa8f41ed68b550843aaad26dec49bfe66ee
Claire Hanson <clairecatohanson@gmail.com>
2023-11-15 11:27:03 -0600

    Updated main and kiln after adding steps to fire the pottery in the kiln

---
+++ src/scripts/Kiln.js
@@ -0,0 +1,10 @@
+export const firePottery = (potteryObject, temp) => {
+    potteryObject.fired = true
+    if (temp > 2200) {
+        potteryObject.cracked = true
+    }
+    else {
+        potteryObject.cracked = false
+    }
+    return potteryObject
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 // Imports go first
 import { makePottery } from './PotteryWheel.js'
+import { firePottery } from './Kiln.js'
 
 // Make 5 pieces of pottery at the wheel
 let mug = makePottery("Mug", 1, 4)
@@ -10,7 +11,12 @@ let dinnerPlate = makePottery("Dinner Plate", 3, 1)
 console.log(mug, soupBowl, ramenBowl, vase, dinnerPlate)
 
 // Fire each piece of pottery in the kiln
-
+mug = firePottery(mug, 2200)
+soupBowl = firePottery(soupBowl, 2000)
+ramenBowl = firePottery(ramenBowl, 2500)
+vase = firePottery(vase, 2800)
+dinnerPlate = firePottery(dinnerPlate, 2100)
+console.log(mug, soupBowl, ramenBowl, vase, dinnerPlate)
 
 // Determine which ones should be sold, and their price
 
